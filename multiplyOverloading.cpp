255bc9de804725961f9efdf8413783d4738a457a
Araj Dulal <sendmailtoaraj@gmail.com>
2023-09-21 21:21:41 +0545

    * operator overloading

---
+++ multiplyOverloading.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class ovrld{
+	private:
+		int a;
+	public:
+		void get()
+		{
+			cout<<"Enter a : "<<endl;
+			cin>>a;
+			
+		}
+		void display()
+		{
+			cout<<"The value is : "<<a<<endl;
+		}
+		ovrld operator +(ovrld obj2)
+		{
+			ovrld obj1;
+			obj1.a=a*obj2.a;
+			return obj1;
+		}
+};
+int main()
+{
+	ovrld x,y,z;
+	y.get();
+	z.get();
+	x=y*z;
+	y.display();
+	z.display();
+	x.display();
+	return 0;
+}
